15aadd5c38075012e06ba3294b5c4f0a5ce51ced
Kenny Chandra <43095847+knychandra@users.noreply.github.com>
2018-11-08 13:31:53 +0700

    Add files via upload

---
+++ new folder/init(part1).py
@@ -0,0 +1,20 @@
+class Student:
+
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+john = Student('John', 19, 85)
+james = Student('James', 21, 95)
+
+# printing john's attributes
+print(john.age)
+print(john.age)
+print(john.grade)
+
+# printing james' attributes
+
+print(james.name)
+print(james.age)
+print(john.grade)
